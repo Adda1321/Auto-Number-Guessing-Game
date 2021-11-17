a3fe58149832742771c9a94027eb6cc9a981c9cc
Adda1321 <adilmustafa13@gmail.com>
2021-11-17 11:44:00 +0500

    Update GameOver.js

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -5,12 +5,15 @@ function GameOver(props) {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!!</Text>
-      <View  style={styles.ImgContain}>
-       <Image
-        style={styles.Img}
-        resizeMode="contain"
-        source={require('../android/assets/success.png')}
-      /> 
+      <View style={styles.ImgContain}>
+        <Image
+          style={styles.Img}
+          resizeMode="cover"
+          //source={require('../android/assets/success.png')}
+          source={{
+            uri: 'https://cosmosmagazine.com/wp-content/uploads/2020/02/190218-mount-full-1440x810.jpg',
+          }}
+        />
       </View>
       <Text>Number of Rounds : {props.roundNumbers}</Text>
       <Button title="Restart" onPress={props.onRestart} />
@@ -27,19 +30,15 @@ const styles = StyleSheet.create({
   Img: {
     width: '100%',
     height: '100%',
-    
-
   },
-  ImgContain:{
+  ImgContain: {
     width: '80%',
-    height: 300,
-    borderRadius:200,
-    marginVertical:13,
-    overflow:'hidden',
-    borderWidth:3,
-    borderColor:'black',
-    backgroundColor:'blue',
-
-  }
+    height: 310,
+    borderRadius: 200,
+    marginVertical: 13,
+    overflow: 'hidden',
+    borderWidth: 3,
+    borderColor: 'black',
+  },
 });
 export default GameOver;
